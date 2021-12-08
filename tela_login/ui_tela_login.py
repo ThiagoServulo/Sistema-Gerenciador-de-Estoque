0c10c024e7856d653028d74f02b7b584701f97ff
ThiagoServulo <thiago.servulo@sga.pucminas.br>
2021-12-08 10:27:31 -0300

    Criada a tela principal do programa

--- tela_login/ui_tela_login.py
+++ tela_login/ui_tela_login.py
@@ -3,6 +3,7 @@ from PySide2.QtGui import *
 from PySide2.QtWidgets import *
 from PySide2 import QtWidgets
 
+
 class Ui_tela_login(object):
     def setupUi(self, tela_login):
         if not tela_login.objectName():
